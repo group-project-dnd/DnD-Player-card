066fd1ba5d995c6cf2b0fcdfa167b7570db0baa7
HCoggers <harry.cogswell@gmail.com>
2019-01-28 14:44:39 -0800

    starting js

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 'use strict';
+//----------------DATA----------------
 
+var username = prompt('what is your name?');
+var races = ['Dwarf', 'Elf', 'Halfling', 'Human', 'Dragonborn', 'Gnome', 'Half-Elf', 'Half-Orc', 'Tiefling'];
+var jobs = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorceror', 'Warlock', 'Wizard'];
+
+
+//----------------FUNCTION DECLARATIONS----------------
+
+function Character(name, race, job) {
+  this.name = name;
+  this.race = race;
+  this.job = job;
+
+}
+
+//----------------FUNCTION INVOCATIONS----------------
