088da68d4fcd480f10f8f3b7e54421c4384bb3fe
HCoggers <harry.cogswell@gmail.com>
2019-01-28 15:11:34 -0800

    partial progress

--- app.js
+++ app.js
@@ -4,7 +4,9 @@
 var username = prompt('what is your name?');
 var races = ['Dwarf', 'Elf', 'Halfling', 'Human', 'Dragonborn', 'Gnome', 'Half-Elf', 'Half-Orc', 'Tiefling'];
 var jobs = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorceror', 'Warlock', 'Wizard'];
+var backgrounds = ['Acolyte', 'Charlatan', 'Criminal'];
 
+var allChars = [];
 
 //----------------FUNCTION DECLARATIONS----------------
 
@@ -12,7 +14,20 @@ function Character(name, race, job) {
   this.name = name;
   this.race = race;
   this.job = job;
-
+  this.background = '';
+  this.alignment = '';
+  allChars.push(this);
 }
 
 //----------------FUNCTION INVOCATIONS----------------
+
+var sub = document.getElementsByClassName('sub');
+for(var i = 0; i < sub.length; i++) {
+  sub[i].style.visibility = hidden;
+}
+
+//Assigns background
+// for (var i = 0; i < allChars.length; i++)
+//   switch(allChars[i].race) {
+//     case jobs[0]: allChars[i].background = 
+//   }
