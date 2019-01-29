75f06b0248bfceb026a74e75a52aede46d9f4468
HCoggers <harry.cogswell@gmail.com>
2019-01-28 19:47:49 -0800

    health and modifier functions

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var races = ['Dwarf', 'Elf', 'Halfling', 'Human', 'Dragonborn', 'Gnome', 'Half-E
 var jobs = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorceror', 'Warlock', 'Wizard'];
 var backgrounds = ['Acolyte', 'Charlatan', 'Criminal', 'Entertainer', 'Folk Hero', 'Guild Artisan', 'Hermit', 'Noble', 'Outlander', 'Sage', 'Sailor', 'Soldier'];
 var subraces = ['Hill Dwarf', 'Mountain Dwarf', 'High Elf', 'Wood Elf', 'Dark Elf (Drow)', 'Lightfoot', 'Stout', 'Forest Gnome', 'Rock Gnome'];
+var proficiencies = ['Athletics', 'Acrobatics', 'Sleight of Hand', 'Stealth', 'Arcana', 'History', 'Investigation', 'Nature', 'Religion', 'Animal Handling', 'Insight', 'Medicine', 'Perception', 'Survival', 'Deception', 'Intimidation', 'Performance', 'Persuasion'];
 
 var allChars = [];
 
@@ -18,146 +19,206 @@ function Character(name, race, subrace, job) {
   this.job = job;
   this.background = '';
   this.hp = 0;
-  this.hitdice = '';
-  this.ablscores = [0, 0, 0, 0, 0, 0];
+  this.hitdie = '';
+  this.proBonus = 2;
+  this.inspiration = 0;
+  this.ablScores = [];
   this.modifiers = [0, 0, 0, 0, 0, 0];
-  this.skillpros = [];
+  this.ablPros = [];
   allChars.push(this);
 }
 
-//Assigns background
-function assignBackground () {
+//Assigns background, hit dice, and hp
+function assignBackgroundHealth() {
   for (var i = 0; i < allChars.length; i++) {
-    switch(allChars[i].job) {
-      case jobs[0]: allChars[i].background = backgrounds[8];
-      case jobs[1]: allChars[i].background = backgrounds[3];
-      case jobs[2]: allChars[i].background = backgrounds[0];
-      case jobs[3]: allChars[i].background = backgrounds[6];
-      case jobs[4]: allChars[i].background = backgrounds[11];
-      case jobs[5]: allChars[i].background = backgrounds[0];
-      case jobs[6]: allChars[i].background = backgrounds[4];
-      case jobs[7]: allChars[i].background = backgrounds[8];
-      case jobs[8]: allChars[i].background = backgrounds[2];
-      case jobs[9]: allChars[i].background = backgrounds[9];
-      case jobs[10]: allChars[i].background = backgrounds[1];
-      case jobs[11]: allChars[i].background = backgrounds[5];
+    switch (allChars[i].job) {
+      case jobs[0]:
+        allChars[i].background = backgrounds[8];
+        allChars[i].ablPros.push()
+        allChars[i].hitdie = 'd12';
+        allChars[i].hp = 12;
+        break;
+      case jobs[1]:
+        allChars[i].background = backgrounds[3];
+        allChars[i].hitdie = 'd8';
+        allChars[i].hp = 8;
+        break;
+      case jobs[2]:
+        allChars[i].background = backgrounds[0];
+        allChars[i].hitdie = 'd8';
+        allChars[i].hp = 8;
+        break;
+      case jobs[3]:
+        allChars[i].background = backgrounds[6];
+        allChars[i].hitdie = 'd8';
+        allChars[i].hp = 8;
+        break;
+      case jobs[4]:
+        allChars[i].background = backgrounds[11];
+        allChars[i].hitdie = 'd10';
+        allChars[i].hp = 10;
+        break;
+      case jobs[5]:
+        allChars[i].background = backgrounds[0];
+        allChars[i].hitdie = 'd8';
+        allChars[i].hp = 8;
+        break;
+      case jobs[6]:
+        allChars[i].background = backgrounds[4];
+        allChars[i].hitdie = 'd10';
+        allChars[i].hp = 10;
+        break;
+      case jobs[7]:
+        allChars[i].background = backgrounds[8];
+        allChars[i].hitdie = 'd10';
+        allChars[i].hp = 10;
+        break;
+      case jobs[8]:
+        allChars[i].background = backgrounds[2];
+        allChars[i].hitdie = 'd8';
+        allChars[i].hp = 8;
+        break;
+      case jobs[9]:
+        allChars[i].background = backgrounds[9];
+        allChars[i].hitdie = 'd6';
+        allChars[i].hp = 6;
+        break;
+      case jobs[10]:
+        allChars[i].background = backgrounds[1];
+        allChars[i].hitdie = 'd8';
+        allChars[i].hp = 8;
+        break;
+      case jobs[11]:
+        allChars[i].background = backgrounds[5];
+        allChars[i].hitdie = 'd6';
+        allChars[i].hp = 6;
+        break;
+    }
+  }
+}
+//Assigns ability scores/skill proficiencies
+function assignAblScores() {
+  for (var i = 0; i < allChars.length; i++) {
+    switch (allChars[i].job) {
+      case jobs[0]:
+        allChars[i].ablScores = [15, 13, 14, 10, 12, 8];
+        allChars[i].ablPros.push()
+        break;
+      case jobs[1]:
+        allChars[i].ablScores = [8, 14, 10, 13, 12, 15];
+        break;
+      case jobs[2]:
+        allChars[i].ablScores = [12, 10, 14, 13, 15, 8];
+        break;
+      case jobs[3]:
+        allChars[i].ablScores = [10, 12, 14, 13, 15, 8];
+        break;
+      case jobs[4]:
+        allChars[i].ablScores = [15, 12, 14, 10, 13, 8];
+        break;
+      case jobs[5]:
+        allChars[i].ablScores = [8, 15, 13, 10, 14, 12];
+        break;
+      case jobs[6]:
+        allChars[i].ablScores = [14, 10, 15, 8, 12, 13];
+        break;
+      case jobs[7]:
+        allChars[i].ablScores = [10, 15, 13, 12, 14, 8];
+        break;
+      case jobs[8]:
+        allChars[i].ablScores = [8, 15, 10, 13, 12, 14];
+        break;
+      case jobs[9]:
+        allChars[i].ablScores = [8, 14, 12, 13, 10, 15];
+        break;
+      case jobs[10]:
+        allChars[i].ablScores = [8, 13, 12, 14, 10, 15];
+        break;
+      case jobs[11]:
+        allChars[i].ablScores = [8, 13, 10, 15, 14, 12];
+        break;
+    }
+  }
+  for (var i = 0; i < allChars.length; i++) {
+    switch (allChars[i].race) {
+      case races[0]:
+        allChars[i].ablScores[2] += 2;
+        break;
+      case races[1]:
+        allChars[i].ablScores[1] += 2;
+        break;
+      case races[2]:
+        allChars[i].ablScores[1] += 2;
+        break;
+      case races[3]:
+        allChars[i].ablScores[0]++;
+        allChars[i].ablScores[1]++;
+        allChars[i].ablScores[2]++;
+        allChars[i].ablScores[3]++;
+        allChars[i].ablScores[4]++;
+        allChars[i].ablScores[5]++;
+        break;
+      case races[4]:
+        allChars[i].ablScores[0] += 2;
+        allChars[i].ablScores[5]++;
+        break;
+      case races[5]:
+        allChars[i].ablScores[3] += 2;
+        break;
+      case races[6]:
+        allChars[i].ablScores[5] += 2;
+        allChars[i].ablScores[4]++;
+        allChars[i].ablScores[1]++;
+        break;
+      case races[7]:
+        allChars[i].ablScores[0] += 2;
+        allChars[i].ablScores[2]++;
+        break;
+      case races[8]:
+        allChars[i].ablScores[5] += 2;
+        allChars[i].ablScores[3]++;
+        break;
+    }
+    switch (allChars[i].subrace) {
+      case subraces[0]:
+        allChars[i].ablScores[4]++;
+        allChars[i].hp++;
+        break;
+      case subraces[1]:
+        allChars[i].ablScores[0] += 2;
+        break;
+      case subraces[2]:
+        allChars[i].ablScores[3]++;
+        break;
+      case subraces[3]:
+        allChars[i].ablScores[4]++;
+        break;
+      case subraces[4]:
+        allChars[i].ablScores[5]++;
+        break;
+      case subraces[5]:
+        allChars[i].ablScores[5]++;
+        break;
+      case subraces[6]:
+        allChars[i].ablScores[2]++;
+        break;
+      case subraces[7]:
+        allChars[i].ablScores[1]++;
+        break;
+      case subraces[8]:
+        allChars[i].ablScores[2]++;
+        break;
+      default:
+        break;
     }
   }
 }
 
-//Assigns ability scores
-function assignAblscores() {
+//Calculates ability modifiers
+function calcModifiers() {
   for (var i = 0; i < allChars.length; i++) {
-    switch(allChars[i].job) {
-      case jobs[0]: 
-        allChars[i].ablscores = [15, 13, 14, 10, 12, 8];
-        break;
-      case jobs[1]: 
-        allChars[i].ablscores = [8, 14, 10, 13, 12, 15];
-        break;
-      case jobs[2]: 
-        allChars[i].ablscores = [12, 10, 14, 13, 15, 8];
-        break;
-      case jobs[3]: 
-        allChars[i].ablscores = [10, 12, 14, 13, 15, 8];
-        break;
-      case jobs[4]: 
-        allChars[i].ablscores = [15, 12, 14, 10, 13, 8];
-        break;
-      case jobs[5]: 
-        allChars[i].ablscores = [8, 15, 13, 10, 14, 12];
-        break;
-      case jobs[6]: 
-        allChars[i].ablscores = [14, 10, 15, 8, 12, 13];
-        break;
-      case jobs[7]: 
-        allChars[i].ablscores = [10, 15, 13, 12, 14, 8];
-        break;
-      case jobs[8]: 
-        allChars[i].ablscores = [8, 15, 10, 13, 12, 14];
-        break;
-      case jobs[9]: 
-        allChars[i].ablscores = [8, 14, 12, 13, 10, 15];
-        break;
-      case jobs[10]: 
-        allChars[i].ablscores = [8, 13, 12, 14, 10, 15];
-        break;
-      case jobs[11]: 
-        allChars[i].ablscores = [8, 13, 10, 15, 14, 12];
-        break;
-    }
-  }
-  for (var i = 0; i < allChars.length; i++) {
-    switch(allChars[i].race) {
-      case races[0]: 
-        allChars[i].ablscores[2] += 2;
-        break;
-      case races[1]: 
-        allChars[i].ablscores[1] += 2;
-        break;
-      case races[2]: 
-        allChars[i].ablscores[1] += 2;
-        break;
-      case races[3]: 
-        allChars[i].ablscores[0]++;
-        allChars[i].ablscores[1]++;
-        allChars[i].ablscores[2]++;
-        allChars[i].ablscores[3]++;
-        allChars[i].ablscores[4]++;
-        allChars[i].ablscores[5]++;
-        break;
-      case races[4]: 
-        allChars[i].ablscores[0] += 2;
-        allChars[i].ablscores[5]++;
-        break;
-      case races[5]: 
-        allChars[i].ablscores[3] += 2;
-        break;
-      case races[6]: 
-        allChars[i].ablscores[5] += 2;
-        allChars[i].ablscores[4]++;
-        allChars[i].ablscores[1]++;
-        break;
-      case races[7]: 
-        allChars[i].ablscores[0] += 2;
-        allChars[i].ablscores[2]++;
-        break;
-      case races[8]: 
-        allChars[i].ablscores[5] += 2;
-        allChars[i].ablscores[3]++;
-        break;
-    }
-    switch(allChars[i].subrace) {
-      case subraces[0]: 
-        allChars[i].ablscores[4]++;
-        allChars[i].hp++;
-        break;
-      case subraces[1]: 
-        allChars[i].ablscores[0] += 2;
-        break;
-      case subraces[2]: 
-        allChars[i].ablscores[3]++;
-        break;
-      case subraces[3]: 
-        allChars[i].ablscores[4]++;
-        break;
-      case subraces[4]: 
-        allChars[i].ablscores[5]++;
-        break;
-      case subraces[5]: 
-        allChars[i].ablscores[5]++;
-        break;
-      case subraces[6]: 
-        allChars[i].ablscores[2]++;
-        break;
-      case subraces[7]: 
-        allChars[i].ablscores[1]++;
-        break;
-      case subraces[8]: 
-        allChars[i].ablscores[2]++;
-        break;
-      default:
-      break;
+    for (var e = 0; e < allChars[i].ablScores.length; e++) {
+      allChars[i].modifiers[e] = Math.floor((allChars[i].ablScores[e] - 10)/2);
     }
   }
 }
@@ -169,13 +230,13 @@ new Character('Harry', 'Tiefling', '', 'Wizard');
 new Character('Aaron', 'Half-Orc', '', 'Fighter');
 new Character('Leo', 'Dragonborn', 'White', 'Rogue');
 new Character('Elizabeth', 'Halfling', 'Lightfoot', 'Cleric');
-assignBackground();
-assignAblscores();
-
+assignBackgroundHealth();
+assignAblScores();
+calcModifiers();
+console.log(allChars);
 
 //hides subraces on page load
 var sub = document.getElementsByClassName('sub');
-for(var i = 0; i < sub.length; i++) {
+for (var i = 0; i < sub.length; i++) {
   sub[i].style.display = 'none';
 }
-
