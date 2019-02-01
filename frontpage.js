a289de6ceaabac93035187a63a5f2dffe08ff087
HCoggers <harry.cogswell@gmail.com>
2019-01-31 18:07:14 -0800

    saves one character at a time

--- frontpage.js
+++ frontpage.js
@@ -3,6 +3,15 @@ var submit = document.getElementById('submit');
 var player = document.getElementById('username');
 var anchor = document.getElementById('proceed')
 
+// load code
+
+var load = JSON.parse(localStorage.getItem(localStorage.key(0)));
+
+if (localStorage.key(0) === "newChar") {
+  document.getElementById('load').style.display = 'block';
+}
+
+//load code end
 
 function handleSubmit(event){
   window.localStorage.clear();
